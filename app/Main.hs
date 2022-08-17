12ad5576b0ab891aca71e7a0cfa43cf7cad08e01
Eavolution <afkrobertson@proton.me>
2022-08-17 22:27:26 +0100

    Introduced handling of decimal

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,7 @@ import System.Environment
 putHelpText :: IO()
 putHelpText = do
     name <- getProgName
+    putStrLn $ name ++ ": adds commas to numbers to make them more readable. Written by Eavolution."
     putStrLn $ "Usage: " ++ name ++ " optional <number> or recieve from STDIN."
 
 main :: IO ()
@@ -15,9 +16,9 @@ main = do
     
     case length args of
         -- If none get from stdin (to allow for piping) then put the comma'd version to stdout
-        0 -> (getLine >>= (\s -> putStrLn $ comma s))
+        0 -> (getLine >>= (\s -> putStrLn $ commaGl s))
         -- If one argument 
-        1 -> putStrLn $ comma $ head args
+        1 -> putStrLn $ commaGl $ head args
         -- Else output help
         _ -> putHelpText
 
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,29 @@
-module Lib (comma) where
+module Lib (
+            commaEU,
+            commaGl
+           ) where
 
-comma :: String -> String
-comma = reverse . aux . reverse
+commaEU :: String -> String
+commaEU = comma ',' '.'
+
+commaGl :: String -> String
+commaGl = comma '.' ','
+
+splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
+splitAtFirst x = fmap (drop 1) . break (x ==)
+
+comma :: Char -> Char -> String -> String
+comma decimal com string = (commaNoDots com $ fst strings) ++ (decimal : snd strings)
+    where 
+        strings :: (String, String)
+        strings = splitAtFirst decimal string
+
+
+commaNoDots :: Char -> String -> String
+commaNoDots com = reverse . aux . reverse
     where 
         aux :: String -> String
-        aux (x:y:z:w:xs) = x : y : z : ',' : aux (w:xs)
+        aux (x:y:z:w:xs) = x : y : z : com : aux (w:xs)
         aux (x:y:z:xs) = x : y : z : aux xs
         aux (x:y:xs) = x : y : aux xs
         aux (x:xs) = x : xs
