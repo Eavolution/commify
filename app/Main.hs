cde1053a35cf5a2d41e10353c2cb898b35c5614a
Eavolution <afkrobertson@proton.me>
2022-08-18 20:51:56 +0100

    Fix attempt

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
 import Lib
-import ArgParse
+import Argsect
+import Types
 import System.Environment
 
 swHelp = Switch "-h" "--help" "Displays the help menu." :: Switch
@@ -11,25 +12,10 @@ swGl = Switch "-g" "--global"   "Formats the numbers US/UK style ie. '1,000.00'.
 definedSwitches :: [Switch]
 definedSwitches = [swHelp, swEu, swGl]
 
-putHelpText :: IO()
-putHelpText = do
-    name <- getProgName
-    putStrLn $ name ++ ": adds commas to numbers to make them more readable. Written by Eavolution."
-    putStrLn $ "Usage: " ++ name ++ " <number> <switches>"
-
 main :: IO ()
 main = do
     -- Get args and figure out how many there are
     cliArgs <- getArgs
 
-    -- Parse the arguments provided
-    let args = parseArgs definedSwitches cmd
-    let switches = snd args
-    let numbers = fst args
 
-    if (swHelp `elem` switches) then
-        putHelpText
-    else
-        if null $ filter (\x -> x /= help) switches
-            print $ commaEU head (fst)
-    print args
+    print $ argsect definedSwitches [] cliArgs
