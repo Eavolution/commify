46c79c5e12298559ef088a49402197b058a387b4
Eavolution <afkrobertson@proton.me>
2022-08-19 17:56:00 +0100

    Implemented custom values

--- app/Main.hs
+++ app/Main.hs
@@ -8,35 +8,67 @@ import Data.Maybe
 swHelp = Switch "-h" "--help" "Displays the help menu." :: Switch
 swEu = Switch "-e" "--european" "Formats the numbers european style ie. '1.000,00'." :: Switch
 swGl = Switch "-g" "--global"   "Formats the numbers US/UK style ie. '1,000.00'." :: Switch
+swSwLc = Switch "-s" "--swiss" "Formats the numbers Swiss/Lichenstein style ie. 1'000.00" :: Switch
 
-usageDescription = "number [optional switches]"
+definedSwitches = [swHelp, swEu, swGl, swSwLc] :: [Switch]
 
-definedSwitches :: [Switch]
-definedSwitches = [swHelp, swEu, swGl]
+swCustom = DataSwitch "-c"
+    "--custom"
+    "., for UK/US 1,000.00" 
+    (\d -> (length d) == 2)
+    False 
+    "Allows the setting of custom seperators in the form decimal followed by thousands seperator\
+    \ ie. .," :: DataSwitch
 
-comma :: [Switch] -> String -> String
-comma switches
-    | swEu `elem` switches = commaEu
-    | swGl `elem` switches = commaGl
-    | otherwise = commaGl
+definedDataSwitches = [swCustom] :: [DataSwitch] 
+
+usageDescription = "number [optional switches]" :: String
 
 main :: IO ()
 main = do
     cliArgs <- getArgs
     progName <- getProgName
-    let parsed = argsect definedSwitches [] cliArgs
+    let parsed = argsect definedSwitches definedDataSwitches cliArgs
+    let usage = "number [optional switches]\nNumbers must have only 0-9 \
+        \and optionally only decimal character of the region (',' for europe, '.' globally)"
+
     -- Check if an error in parsing
     if isJust (snd parsed) then
         putStrLn $ (fromJust (snd parsed)) progName usageDescription
     else do
-        let args = fromJust $ fst parsed
-        let numbers = aPositionals args
-        let switches = aSwitches args
+        -- Args deemed to be valid, therefore parse them and seperate positionals from switches
 
-        -- Sort out args
-        if swHelp `elem` switches || null numbers then
-            putStrLn "Help"
-            --putStrLn $ defaultHelpText definedSwitches [] progName "number [optional switches]"
-        else 
-            putStrLn "Comma'd value"
-            --putStrLn $ comma switches $ head numbers
+        let args = fromJust $ fst parsed :: Args
+        let numbers = aPositionals args :: [PosArg]
+        let switches = aSwitches args :: [Switch]
+        let dataSwitches = aDataSwitches args :: [DataSwitch]
+
+        putStrLn $ output switches dataSwitches numbers progName usage
+        where
+            output :: [Switch] -> [DataSwitch] -> [PosArg] -> String -> String -> String
+            output sws dsws nums oProgName oUsage
+                -- Display the help menu on -h
+                | swHelp `elem` sws =
+                    defaultHelpText
+                        definedSwitches definedDataSwitches oProgName oUsage
+                -- Require one number unless help was specified, obviously
+                | length nums /= 1 = "1 positional argument required, " ++
+                    show (length nums) ++ "given."
+                -- If the europe switch is present, format number EU style if number is valid
+                | swEu `elem` sws =
+                    fromMaybe errorMsg (commaEu num)
+                -- If the global switch is present, format number UK/US style if number is valid
+                | swGl `elem` sws =
+                    fromMaybe errorMsg (commaGl num)
+                -- Swiss/Lichenstein style
+                | swSwLc `elem` sws =
+                    fromMaybe errorMsg (commaSwLc num)
+                | swCustom `elem` dsws =
+                    fromMaybe errorMsg (comma (head customData) (last customData) num)
+                -- Default to global
+                | otherwise = 
+                    fromMaybe ("Invalid number given: " ++ num) (commaGl num)
+                        where
+                            num = head nums
+                            errorMsg = "Invalid number given: " ++ num
+                            customData = fromJust $ getDataFromDswList (dswIdShort swCustom) dsws
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,28 +1,49 @@
-module Lib (
-            commaEu,
-            commaGl,
-            splitAtFirst
-           ) where
+module Lib 
+(
+    -- Expose common styles
+    commaEu, commaGl, commaSwLc,
+    -- Expose base function, so new styles can be added
+    comma
+) where
 
--- Use '.' as thousand seperator and ',' as decimal point. European.
-commaEu :: String -> String
+-- Use '.' as thousand seperator and ',' as decimal. European. 1.000,00
+commaEu :: String -> Maybe String
 commaEu = comma ',' '.'
 
--- Use ',' as thousand seperator and '.' as decimal point. Global.
-commaGl :: String -> String
+-- Use ',' as thousand seperator and '.' as decimal. Global. 1,000.00
+commaGl :: String -> Maybe String
 commaGl = comma '.' ','
+
+-- Switzerland and Lichenstein style. 1'000.00
+commaSwLc :: String -> Maybe String
+commaSwLc = comma '.' '\''
 
 splitAtFirst :: Eq a => a -> [a] -> ([a], [a])
 splitAtFirst x = fmap (drop 1) . break (x ==)
 
-comma :: Char -> Char -> String -> String
-comma decimal com string = ""
---    if decimal `elem` string then
---        (commaNoDots com $ fst $ splitAtFirst decimal string) ++ (decimal : snd $ splitAtFirst decimal string)
- --   else
- --       commaNoDots com string
+-- Puts commas in numbers using the supplied decimal and comma character
+comma :: Char -> Char -> String -> Maybe String
+comma decimal com string
+    | not (checkNumber decimal string) = Nothing
+    | decimal `elem` string = Just $ (commaNoDots com $ fst split) ++ (decimal : (snd split))
+    | otherwise = Just $ commaNoDots com string        
+    where
+        split = splitAtFirst decimal string :: (String, String)
 
+        -- Checks that a number conforms to rules:
+        -- No characters other than number and decimal
+        -- Only one or none decimal character
+        checkNumber :: Char -> String -> Bool
+        checkNumber decimal number 
+            | any (\x -> not (x `elem` acceptable)) number = False
+            | moreThanOne decimal number = False
+            | otherwise = True
+            where
+                acceptable = decimal : ['0'..'9'] :: [Char]
+                moreThanOne :: (Eq a) => a -> [a] -> Bool
+                moreThanOne dec num = length (filter (\x -> x == dec) num) > 1
 
+-- Puts commas in numbers with no decimals
 commaNoDots :: Char -> String -> String
 commaNoDots com = reverse . aux . reverse
     where 
