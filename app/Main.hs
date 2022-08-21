ed9252a15fb1022f6c1751d6882bc8ea38413b96
Eavolution <afkrobertson@proton.me>
2022-08-21 11:38:21 +0100

    Handles no input number properly

--- app/Main.hs
+++ app/Main.hs
@@ -2,73 +2,132 @@ module Main (main) where
 
 import Lib
 import Argsect
+
 import System.Environment
 import Data.Maybe
 
-swHelp = Switch "-h" "--help" "Displays the help menu." :: Switch
-swEu = Switch "-e" "--european" "Formats the numbers european style ie. '1.000,00'." :: Switch
-swGl = Switch "-g" "--global"   "Formats the numbers US/UK style ie. '1,000.00'." :: Switch
-swSwLc = Switch "-s" "--swiss" "Formats the numbers Swiss/Lichenstein style ie. 1'000.00" :: Switch
+swHelp :: Switch
+swHelp =
+    Switch
+    "-h"
+    "--help"
+    "Displays the help menu."
 
-definedSwitches = [swHelp, swEu, swGl, swSwLc] :: [Switch]
+swEu :: Switch
+swEu =
+    Switch
+    "-e"
+    "--european"
+    "Formats the numbers european style ie. '1.000,00'."
 
-swCustom = DataSwitch "-c"
+swGl :: Switch
+swGl =
+    Switch
+    "-g"
+    "--global"
+    "Formats the numbers US/UK style ie. '1,000.00'."
+
+swSwLc :: Switch
+swSwLc =
+    Switch
+    "-s"
+    "--swiss"
+    "Formats the numbers Swiss/Lichenstein style ie. 1'000.00"
+
+swCustom :: DataSwitch
+swCustom =
+    DataSwitch
+    "-c"
     "--custom"
-    "., for UK/US 1,000.00" 
-    (\d -> (length d) == 2)
-    False 
-    "Allows the setting of custom seperators in the form decimal followed by thousands seperator\
-    \ ie. .," :: DataSwitch
+    "., for UK/US 1,000.00"
+    (\ x -> (length x) == 2)
+    False
+    "Allows the setting of custom seperators in the form decimal followed\
+    \by thousands seperator ie. .,"
 
-definedDataSwitches = [swCustom] :: [DataSwitch] 
+definedSwitches :: [Switch]
+definedSwitches = [swHelp, swEu, swGl, swSwLc]
 
-usageDescription = "number [optional switches]" :: String
+definedDataSwitches :: [DataSwitch]
+definedDataSwitches = [swCustom]
+
+usageDescription :: String
+usageDescription = "number [optional switches]\nNumbers must have only 0-9 \
+        \and optionally only decimal character of the region\
+        \(',' for europe, '.' globally)"
+
+-- Chooses which function to call based on if a number was provided or not
+handleSwitches :: Maybe PosArg 
+                    -> [Switch]
+                    -> [DataSwitch]
+                    -> String
+                    -> String
+                    -> IO String
+handleSwitches Nothing = handleSwitchesStdIn
+handleSwitches num = handleSwitchesPure $ fromJust num
+
+-- Produces final output based on what switches are present, try to remove IO
+handleSwitchesPure :: PosArg 
+                    -> [Switch]
+                    -> [DataSwitch]
+                    -> String
+                    -> String
+                    -> IO String
+handleSwitchesPure num sws dsws oProgName oUsage
+    -- Display the help menu on -h
+    | swHelp `elem` sws = return helpText
+    -- If the europe switch is present, format number EU style
+    | swEu `elem` sws =
+        return $ fromMaybe errorMsg (commaEu num)
+    -- If the global switch is present, format number UK/US style
+    | swGl `elem` sws =
+        return $ fromMaybe errorMsg (commaGl num)
+    -- Swiss/Lichenstein style
+    | swSwLc `elem` sws =
+        return $ fromMaybe errorMsg (commaSwLc num)
+    | swCustom `elem` dsws =
+        return $ fromMaybe errorMsg (comma dec thou num)
+    -- Default to global
+    | otherwise =
+        return $ fromMaybe ("Invalid number given: " ++ num) (commaGl num)
+    where
+        errorMsg = "Invalid number given: " ++ num
+        helpText = prettyHelpText
+            definedSwitches definedDataSwitches oProgName oUsage
+        customData =
+            fromJust $ getDataFromDswList (dswIdShort swCustom) dsws
+        dec = head customData
+        thou = last customData
+
+-- If no number was provided, it is got from STDIN and passed along
+handleSwitchesStdIn :: [Switch]
+                    -> [DataSwitch]
+                    -> String
+                    -> String
+                    -> IO String
+handleSwitchesStdIn sws dsws progName usage = do
+    num <- getLine
+    handleSwitchesPure num sws dsws progName usage
 
 main :: IO ()
 main = do
     cliArgs <- getArgs
     progName <- getProgName
+
     let parsed = argsect definedSwitches definedDataSwitches cliArgs
-    let usage = "number [optional switches]\nNumbers must have only 0-9 \
-        \and optionally only decimal character of the region (',' for europe, '.' globally)"
-
-    -- Check if an error in parsing
-    if isJust (snd parsed) then
-        putStrLn $ (fromJust (snd parsed)) progName usageDescription
-    else do
-        -- Args deemed to be valid, therefore parse them and seperate positionals from switches
-
-        let args = fromJust $ fst parsed :: Args
-        let numbers = aPositionals args :: [PosArg]
-        let switches = aSwitches args :: [Switch]
-        let dataSwitches = aDataSwitches args :: [DataSwitch]
-
-        putStrLn $ output switches dataSwitches numbers progName usage
-        where
-            output :: [Switch] -> [DataSwitch] -> [PosArg] -> String -> String -> String
-            output sws dsws nums oProgName oUsage
-                -- Display the help menu on -h
-                | swHelp `elem` sws = helpText
-                -- Require one number unless help was specified, obviously
-                | length nums /= 1 = "1 positional argument required, " ++
-                    show (length nums) ++ " \n" ++ helpText
-                -- If the europe switch is present, format number EU style if number is valid
-                | swEu `elem` sws =
-                    fromMaybe errorMsg (commaEu num)
-                -- If the global switch is present, format number UK/US style if number is valid
-                | swGl `elem` sws =
-                    fromMaybe errorMsg (commaGl num)
-                -- Swiss/Lichenstein style
-                | swSwLc `elem` sws =
-                    fromMaybe errorMsg (commaSwLc num)
-                | swCustom `elem` dsws =
-                    fromMaybe errorMsg (comma (head customData) (last customData) num)
-                -- Default to global
-                | otherwise = 
-                    fromMaybe ("Invalid number given: " ++ num) (commaGl num)
-                        where
-                            num = head nums
-                            errorMsg = "Invalid number given: " ++ num
-                            helpText = defaultHelpText
-                                definedSwitches definedDataSwitches oProgName oUsage
-                            customData = fromJust $ getDataFromDswList (dswIdShort swCustom) dsws
+    
+    (either
+        (\ x -> return $ x progName usageDescription)
+        (\ x ->
+            handleSwitches
+            (head' (aPositionals x))
+        (aSwitches x)
+        (aDataSwitches x)
+        progName
+        usageDescription)
+        parsed)
+        >>= putStrLn
+    where
+        head' :: [a] -> Maybe a
+        head' [] = Nothing
+        head' (x:_) = Just x
